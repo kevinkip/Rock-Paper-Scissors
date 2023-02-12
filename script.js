bd386358768a9e07aa33bb9565295b6fc1320136
Kevin Kipkemboi <4940465+kevinkip@users.noreply.github.com>
2023-02-12 14:00:15 -0800

    winner and loser script is working

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
-// document.getElementById('rock').onclick = () => {alert("You've chosen rock")};
-const rock = document.getElementById('rock');
-rock.addEventListener('click', choice);
-const playerChoice = document.querySelector('.playerSelection');
 
-document.getElementById('paper').onclick = () => {alert("You've chosen paper")};
-document.getElementById('scissors').onclick = () => {alert("You've chosen scissors")};
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+const outcome = document.querySelector('.outcome');
+
+let resetButton;
+
+// function getComputerChoice(){
+//     let choice = Math.floor(Math.random() * choices.length);
+//     return choices[choice];
+// }
+
+// console.log(getComputerChoice());
+
+// function getPlayerChoice(){
+//     const userGuess = guessField.value.toLowerCase();
+//     let firstLetter = userGuess.slice(0,1);
+//     let restOfString = userGuess.slice(1);
+//     let newString = firstLetter.toUpperCase() + restOfString;
+//     return newString;
+// }
 
 
-function choice(){
-    if(rock){
-        alert("You've chosen rock!");
-        playerChoice.textContent = 'Rock'
+function oneRound(){
+    const userGuess = guessField.value.toLowerCase();
+    let firstLetter = userGuess.slice(0,1);
+    let restOfString = userGuess.slice(1);
+    let playerSelection = firstLetter.toUpperCase() + restOfString;
+    console.log(playerSelection);
+
+    let choice = Math.floor(Math.random() * choices.length);
+    let computerSelection = choices[choice];
+    console.log(computerSelection);
+
+    if(playerSelection == 'Rock' && computerSelection == 'Rock' || 
+    playerSelection == 'Paper' && computerSelection == 'Paper' ||
+    playerSelection == 'Scissors' && computerSelection == 'Scissors'){
+        outcome.textContent = "It's a tie.";
+    }
+
+    if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+        outcome.textContent = "You Lose! Rock loses to Paper";
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
+        outcome.textContent = "You Win! Rock beats Paper";
+    }
+
+    if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        outcome.textContent = "You Lose! Scissors loses to Paper";
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper'){
+        outcome.textContent = "You Win! Scissors beats Paper";
+    }
+
+    if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        outcome.textContent = "You Lose! Scissors loses to Rock";
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        outcome.textContent = "You Win! Rock beats Scissors";
     }
 }
-const winner = document.querySelector('.winner');
 
-
-function getComputerChoice(){
-    const randomPick = Math.floor(Math.random() * choices.length);
-    return (choices[randomPick]);
-};
-
-console.log(getComputerChoice());
-
-function singleRound(playerSelection, computerSelection){
-}
-
-
+guessSubmit.addEventListener('click', oneRound);
