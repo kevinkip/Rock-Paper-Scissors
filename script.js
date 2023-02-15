36cb3bbb4a468a7247b65d643401c4dd0909c367
Kevin Kipkemboi <4940465+kevinkip@users.noreply.github.com>
2023-02-15 05:18:49 -0800

    add more divs and stop js after game over

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 const outcome = document.querySelector('.outcome');
 const gameRounds = document.querySelector('.gameRounds');
+const playerPoints = document.querySelector('.pp');
+const computerPoints = document.querySelector('.cp');
 
 let resetButton;
 let gameCount = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function playerChoice(){
     const userGuess = guessField.value.toLowerCase();
@@ -34,6 +38,8 @@ function oneRound(){
     playerSelection == 'Scissors' && computerSelection == 'Scissors'){
         roundResult = 'tie';
         gameCount+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "It's a tie.";
         gameRounds.textContent = gameCount;
 
@@ -42,12 +48,18 @@ function oneRound(){
     if (playerSelection == 'Rock' && computerSelection == 'Paper'){
         roundResult = 'computer';
         gameCount+=1;
+        computerScore+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "You Lose! Rock loses to Paper";
         gameRounds.textContent = gameCount;
 
     } else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
         roundResult = 'player';
         gameCount+=1;
+        playerScore+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "You Win! Rock beats Paper";
         gameRounds.textContent = gameCount;
 
@@ -56,12 +68,18 @@ function oneRound(){
     if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
         roundResult = 'computer';
         gameCount+=1;
+        computerScore+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "You Lose! Paper loses to Scissors";
         gameRounds.textContent = gameCount;
 
     } else if (playerSelection == 'Scissors' && computerSelection == 'Paper'){
         roundResult = 'player';
         gameCount+=1;
+        playerScore+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "You Win! Scissors beats Paper";
         gameRounds.textContent = gameCount;
 
@@ -70,12 +88,18 @@ function oneRound(){
     if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
         roundResult = 'computer';
         gameCount+=1;
+        computerScore+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "You Lose! Scissors loses to Rock";
         gameRounds.textContent = gameCount;
 
     } else if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
         roundResult = 'player';
         gameCount+=1;
+        playerScore+=1;
+        playerPoints.textContent = playerScore;
+        computerPoints.textContent = computerScore;
         outcome.textContent = "You Win! Rock beats Scissors";
         gameRounds.textContent = gameCount;
 
@@ -88,12 +112,23 @@ function oneRound(){
 // Need a reset button
 // Also a reset game to clear everything back to start.
 
+function gameOver(){
+    guessField.value = '';
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+}
+
 function start(){
     if(gameCount < 5){
         guessSubmit.addEventListener('click', oneRound);
     } else {
-        outcome.textContent = "Game Over!";
-        document.getElementsByClassName('guessSubmit').disabled = true;
+        if(playerScore > computerScore){
+            outcome.textContent = "Player Wins!";
+            gameOver();
+        } else if(computerScore > playerScore){
+            outcome.textContent = "Computer Wins!";
+            gameOver();
+        }
     }
 
 }
